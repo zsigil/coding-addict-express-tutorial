2e0a4647729672518c3104e9628a3e2156652f19
Zsigil <chickpox@freemail.hu>
2021-10-12 15:18:51 +0200

    api: return only part of data

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const { products } = require("./data");
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello Moto</h1><a href="/api/products">products</a>`);
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, image, name } = product;
+    return { id, image, name };
+  });
+  res.json(newProducts);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
