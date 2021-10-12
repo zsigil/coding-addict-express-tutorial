c0f878276fc2b77f5942f89ee82877ee0b1c6502
Zsigil <chickpox@freemail.hu>
2021-10-12 11:03:00 +0200

    setting up static files

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+
+const path = require("path");
 const port = 5000;
 
+//setting up static files and middleware
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
-  res.send("Home page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.get("/about", (req, res) => {
   res.status(200).send("About page");
 });
 
-app.all("/*", (req, res) => {
+app.all("*", (req, res) => {
   res.status(404).send("<h1>Sorry, page not found</h1>");
 });
 
