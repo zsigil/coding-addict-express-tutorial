86b6d343e0b6a99a80a0edb0c58d105ecf417ece
Zsigil <chickpox@freemail.hu>
2021-10-18 14:53:55 +0200

    POST -basic form usage

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const port = 5000;
 
 const { people } = require("./data");
 
+app.use(express.static("./methods-public"));
+
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome, ${name}`);
+  }
+  res.status(401).send("please provide credentials");
+});
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ result: "success", data: people });
 });
