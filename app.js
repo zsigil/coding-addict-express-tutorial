8a9e805e0843699401949dea572f0ef863e497d9
Zsigil <chickpox@freemail.hu>
2021-10-20 10:09:45 +0200

    LOGIN route separately

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 app.use(express.static("./methods-public"));
 
@@ -12,14 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/people", people);
-
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome, ${name}`);
-  }
-  res.status(401).send("please provide credentials");
-});
+app.use("/", auth);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
