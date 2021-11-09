fa13a90bc91aba56d3ba86a5278d046e637fc4bf
Zsigil <chickpox@freemail.hu>
2021-11-09 13:56:48 +0100

    registering user, jwt in response

--- JWT/routes/users.js
+++ JWT/routes/users.js
@@ -11,6 +11,32 @@ router.get("/protected", (req, res, next) => {});
 router.post("/login", function (req, res, next) {});
 
 // TODO
-router.post("/register", function (req, res, next) {});
+router.post("/register", function (req, res, next) {
+  const saltHash = utils.genPassword(req.body.password);
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({
+    user: req.body.username,
+    salt: salt,
+    hash: hash,
+  });
+
+  newUser
+    .save()
+    .then((user) => {
+      const jwt = utils.issueJWT(user);
+
+      res.json({
+        success: true,
+        user: user,
+        token: jwt.token,
+        expiresIn: jwt.expires,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
